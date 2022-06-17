3be7ce965a4ee81c30cdbcfa4da99bc4dc6daf97
M7Tube <clashroyale.mahh@gmail.com>
2022-06-17 18:49:32 +0300

    rk

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\AppSettings;
 use App\Models\Statistics;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('migrate', function () {
+    Artisan::call('migrate:refresh --seed');
+    return true;
+});
+
